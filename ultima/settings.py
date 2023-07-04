fded74f88fc1f7d5561e7b27379577b6e8e773bd
Gileno Filho <contato@gilenofilho.com.br>
2023-07-04 20:07:29 -0300

    aula de modelviewset

--- ultima/settings.py
+++ ultima/settings.py
@@ -113,7 +113,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-# REST_FRAMEWORK = {
-#     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-#     'PAGE_SIZE': 20
-# }
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DATE_INPUT_FORMATS': ["%d/%m/%Y"],
+    'DATE_FORMAT': '%d/%m/%Y',
+}
